f15c0aa0e721acd8dfbd1b464dfb945a2b16cd5b
Raymond Zhang <zhanghui1717@hotmail.com>
2020-01-07 23:38:20 +0800

    Adding ConsumingRestApplication

---
+++ src/main/java/com/example/consumingrest/Quote.java
@@ -0,0 +1,28 @@
+package com.example.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Quote {
+    private String type;
+    private Value value;
+
+    public Quote() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
